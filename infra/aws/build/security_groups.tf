906a1c3a6a81610a423ee07544e4bdbb8cd81fc3
Surfraz Ahmed <surfraz@gmail.com>
2016-12-11 19:46:22 +0000

    add missing access for gpg hkp protocol for key retrieval

--- infra/aws/build/security_groups.tf
+++ infra/aws/build/security_groups.tf
@@ -93,4 +93,11 @@ resource "aws_security_group" "wordpress-webserver-security-group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 11371
+    to_port     = 11371
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
