bd2a291ab46973a5eba6fd1c9a50a03aee633215
Surfraz Ahmed <surfraz@gmail.com>
2016-12-11 12:16:05 +0000

    automatically replace invalid characters from rds db name

--- infra/aws/build/database.tf
+++ infra/aws/build/database.tf
@@ -1,9 +1,9 @@
 resource "aws_db_instance" "wordpress-rds-db" {
-  publicly_accessible  = true
-  allocated_storage    = 10
-  engine               = "mysql"
-  instance_class       = "db.t1.micro"
-  name                 = "${var.name_prefix}_${var.environment}_wordpress"
-  username             = "${var.wordpress_db_user}"
-  password             = "${var.wordpress_db_password}"
+  publicly_accessible = true
+  allocated_storage   = 10
+  engine              = "mysql"
+  instance_class      = "db.t1.micro"
+  name                = "${replace(var.name_prefix, "/[-_\\.@]/", "")}${replace(var.environment, "/[-_\\.@]/", "")}wordpress"
+  username            = "${var.wordpress_db_user}"
+  password            = "${var.wordpress_db_password}"
 }
